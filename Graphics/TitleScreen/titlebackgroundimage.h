4e8dda78add045204d97ae8f3bfd28c22abed919
Darrman <30606600+Darrman@users.noreply.github.com>
2020-07-17 15:44:42 +0100

    Fixed minimug box breaking
    The cause was including the title screen file inside a push/pop it shouldn't have been in. Thankfully buildfiles easily let me undo the damage.

--- Graphics/TitleScreen/titlebackgroundimage.h
+++ Graphics/TitleScreen/titlebackgroundimage.h
@@ -8,7 +8,7 @@
 //	+ 600 tiles not compressed
 //	Total size: 512 + 38400 = 38912
 //
-//	Time-stamp: 2020-03-26, 21:32:18
+//	Time-stamp: 2020-07-17, 03:55:36
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
 //	( http://www.coranac.com/projects/#grit )
 //
